a84d18a09422aedd6acab49c458192ea70fe7d8d
Ankit Yadav <ankityadav.nmims@gmail.com>
2019-04-06 15:54:36 -0400

    Updated NYCAirbnb.R
    Added Code for removing Dollars and Commas from Price and Security_Deposit columns

--- NYCAirbnb.R
+++ NYCAirbnb.R
@@ -4,14 +4,19 @@ install.packages("R.utils")
 library(data.table)
 library(R.utils)
 ### Data Import
-dlistings <- read.csv("listings.csv", stringsAsFactors = F)
+dlistings <- read.csv("/Users/ankityadav/Desktop/Spring_2019/DMPA/Project/Data/April_2018/listings.csv", stringsAsFactors = F)
 str(dlistings)
 
 df <- dlistings
 df <- df[,c(1,23,26:29,32:35,37,39:42,44,46,49:69,71:75,77,80:86,90:92,94:96)]
-
 str(df)
-### removing dollar and comma
+
+### removing dollar and comma ###
 df$extra_people <- as.numeric(gsub("\\$", "", df$extra_people))
-df$price <- as.numeric(gsub("\\$", "", df$price))
-###
+df$price <- gsub("\\$", "", df$price)
+df$price <- as.numeric(gsub(",","",df$price))
+df$security_deposit <- gsub("\\$", "", df$security_deposit)
+df$security_deposit <- as.numeric(gsub(",","",df$security_deposit))
+df$security_deposit <- ifelse(is.na(df$security_deposit), 0, df$security_deposit)
+### 
+
