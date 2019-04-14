eb422b2d548f8f666ed23e9f3800e50807e79e92
Ankit Yadav <ankityadav.nmims@gmail.com>
2019-04-14 15:51:03 -0400

    Updated File

--- NYCAirbnb.R
+++ NYCAirbnb.R
@@ -1,9 +1,10 @@
 ### Libraries
-
+install.packages("tidyverse")
 library(data.table)
 library(dplyr)
 library(R.utils)
 ### Data Import
+
 apr18 <- read.csv("/Users/ankityadav/Desktop/Spring_2019/DMPA/Project/Data/April_2018/listings.csv", stringsAsFactors = F)
 str(apr18)
 
@@ -25,26 +26,9 @@ df$cleaning_fee <- ifelse(is.na(df$cleaning_fee), 0, df$cleaning_fee)
 
 # Included N/A for now, will have to make changes
 df$host_response_time <- factor(df$host_response_time, levels = c("N/A","within an hour", "within a few hours", "within a day","a few days or more"))
-
 df$host_is_superhost <- as.numeric(factor(df$host_is_superhost, levels = c("f","t"))) - 1
 
+df$host_verifications <- factor(df$host_verifications)
 
 
 
-
-# cleaning <- function(df){
-#   df$extra_people <- as.numeric(gsub("\\$", "", df$extra_people))
-#   df$price <- gsub("\\$", "", df$price)
-#   df$price <- as.numeric(gsub(",","",df$price))
-#   df$security_deposit <- gsub("\\$", "", df$security_deposit)
-#   df$security_deposit <- as.numeric(gsub(",","",df$security_deposit))
-#   df$security_deposit <- ifelse(is.na(df$security_deposit), 0, df$security_deposit)
-#   df$cleaning_fee <- gsub("\\$", "", df$cleaning_fee)
-#   df$cleaning_fee <- as.numeric(gsub(",","",df$cleaning_fee))
-#   df$cleaning_fee <- ifelse(is.na(df$cleaning_fee), 0, df$cleaning_fee)
-# }
-# 
-# cleaning(df)
-
-### 
-head(df)
